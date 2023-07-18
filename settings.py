10d8c59749223f299773f6500349880d6c165fbb
Ayushman <gaurmanu71@gmail.com>
2023-07-19 01:37:55 +0530

    first commit

---
+++ settings.py
@@ -0,0 +1,6 @@
+SEARCH_KEY="AIzaSyCsRPDPrgDuWuH2YzttAwBX3m_euIff7gA"
+SEARCH_ID ="80ee42b7b648e4162"
+COUNTRY = "IN"
+
+SEARCH_URL ="https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl="+COUNTRY
+RESULT_COUNT=20
